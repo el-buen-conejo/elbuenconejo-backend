9063405ef2fba54710885a67342aebe055a8c78d
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-02-08 10:49:01 -0600

    Changed structure of the project in the git repository

--- scripts/deploy/nginx.sh
+++ scripts/deploy/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/elbuenconejo-backend/backend/nginx/nginx.conf /etc/nginx/sites-available/rabbits_farm
+sudo cp /home/ubuntu/elbuenconejo-backend/nginx/nginx.conf /etc/nginx/sites-available/rabbits_farm
 sudo ln -s /etc/nginx/sites-available/rabbits_farm /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
