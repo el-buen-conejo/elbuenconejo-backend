0b53b97d7bcae291e6719c37e6c8840bec1ed2a1
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-02-08 16:35:00 -0600

    Corrected production settings

--- scripts/deploy/gunicorn.sh
+++ scripts/deploy/gunicorn.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+export DJANGO_SETTINGS_MODULE=rabbits_farm.prod_settings
 sudo cp /home/ubuntu/elbuenconejo-backend/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/elbuenconejo-backend/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
